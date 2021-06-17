94438dc5b7ccb591cddb9cbaf477852047ffa5d6
John Cabang <johncabz@gmail.com>
2021-06-17 09:50:17 -0400

    Work - post to server/backend - mongoDB

--- client/src/components/AddFriend.js
+++ client/src/components/AddFriend.js
@@ -41,7 +41,7 @@ function AddFriend() {
   const [phoneNumber, setPhoneNumber] = useState(0);
   const [age, setAge] = useState(0);
   const [notes, setNotes] = useState("");
-  // const [listOfFriends, setListOfFriends] = useState([]);
+  const [listOfFriends, setListOfFriends] = useState([]);
 
   const addFriend = () => {
     // console.log(name + email + phoneNumber + age + notes);
@@ -53,7 +53,10 @@ function AddFriend() {
       notes,
     })
       .then(() => {
-        // setListOfFriends([...listOfFriends, {name, email, phoneNumber, age, notes }]);
+        setListOfFriends([
+          ...listOfFriends,
+          { name, email, phoneNumber, age, notes },
+        ]);
         console.log("Friend successfully added");
         history.push("/friends");
       })
